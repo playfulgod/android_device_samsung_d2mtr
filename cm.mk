9bf497d758c43c22a968bc8749661d371641bfc7
David Rouleau <Shabbypenguin@gmail.com>
2012-10-22 19:47:16 -0400

    Edited to reflect d2mtr d2mtr is the MetroPCS Samsung Galaxy S3 model - SCH-R530M

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/d2usc/full_d2usc.mk)
+$(call inherit-product, device/samsung/d2mtr/full_d2mtr.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/cdma.mk)
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2usc TARGET_DEVICE=d2usc BUILD_FINGERPRINT="d2usc-user 4.0.4 IMM76D R530UVXALG1 release-keys" PRIVATE_BUILD_DESC="samsung/d2usc/d2usc:4.0.4/IMM76D/R530UVXALG1:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2mtr TARGET_DEVICE=d2mtr BUILD_FINGERPRINT="d2mtr-user 4.0.4 IMM76D R530MVQALJ1 release-keys" PRIVATE_BUILD_DESC="samsung/d2mtr/d2mtr:4.0.4/IMM76D/R530MVQALJ1:user/release-keys"
 
-PRODUCT_NAME := cm_d2usc
-PRODUCT_DEVICE := d2usc
+PRODUCT_NAME := cm_d2mtr
+PRODUCT_DEVICE := d2mtr
 
--- extract-files.sh
+++ extract-files.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-export DEVICE=d2usc
+export DEVICE=d2mpcs
 export VENDOR=samsung
 ./../d2-common/extract-files.sh $@
