df53a9a64d57362fd820e0cf7ebeab81528a7137
Shareef Ali <shareefalis@gmail.com>
2012-10-30 18:15:17 -0400

    d2mtr: switch to d2vzw location of blobs
    Change-Id: I58d9b76f0670c3d70ee16c93158bee8dfaa242fc

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/d2mtr/d2mtr-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/d2vzw/d2vzw-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2mtr/overlay
